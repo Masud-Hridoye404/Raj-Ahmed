6d1a50c7622d2838c4839ba11119db1aab0d7464
rajmim12626 <rajmim954@gmail.com>
2024-06-13 22:42:35 +0600

    Update npx20.js

--- scripts/commands/npx20.js
+++ scripts/commands/npx20.js
@@ -26,7 +26,7 @@ const media = (
 
 	if (body.indexOf("😊")==0 || body.indexOf("☺️")==0) {
 		var msg = {
-				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "রাজ",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
